08219786025ef4afe14f9f6221903d0f388f9d84
x <>
2023-09-08 13:11:24 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Running a Market Maker on Dexterity
+
+Disclaimer: Unaudited Software - No Liability - As-Is Use
+
+Trading on Dexterity is not allowed in the US and other jurisdictions; you could lose all your money and more with this script.
+
+Please be advised that this software is provided on an "as-is" basis and has not undergone any formal auditing by third-party organizations. We make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability of this software for any purpose. Any reliance you place on such information or use of this software is strictly at your own risk.
+
+We disclaim all liability for losses, damages, or negative consequences you may incur due to the use or misuse of this software, including but not limited to financial loss and data breaches. It is strongly advised that you consult with qualified legal and financial professionals before making any decisions related to the use of this software.
+
+By using this software, you acknowledge that you could potentially lose all your money, data, or other assets, and agree that we will not be liable for any such losses or damages under any circumstances.
+
+# Getting Started
+
+## Requirements
+
+1. git
+2. node version >= 18
+3. (optional) yarn; if you don't use yarn, replace "yarn" with "npm" in all the following commands
+
+Lookup how to install git. [NVM](https://github.com/nvm-sh/nvm) manages node installations for you.
+
+## First-Time Setup
+
+```
+# clone the source code for this market maker
+git clone git@github.com:Hxro-Network/dexterity-mm.git
+cd dexterity-mm
+
+# select a node version of at least 18
+nvm use 18
+
+# install it
+yarn install
+```
+
+## Running the MM
+
+1. You must create a trading account (TRG) and deposit funds before running this script.
+2. Place the private key that owns your TRG inside the cloned dexterity-mm directory. It must be named `my_key.json`. (Or modify the script to accept it as an environment variable).
+3. Run the MM with desired parameters 
+
+The MM quotes around the index price of each product on a Dexterity Market Product Group (MPG).
+
+The MM has the following parameters, which are set via environment variables:
+
+| Key            | Default              | Description                                                                 |
+| -------------- | -------------------- | --------------------------------------------------------------------------- |
+| RPC            | http://localhost:8899/ | -                                                                         |
+| MPG            | 4cKB5xKtDpv4xo6ZxyiEvtyX3HgXzyJUS1Y8hAfoNkMT | Public key of the Dexterity markets (MPG).                                            |
+| TRG            | -                    | Your trading account public key.                                            |
+| QUOTE_PERIOD_MS| 5000                 | How many milliseconds between updating quotes                               |
+| NUM_LEVELS     | 5                    | Number of levels to quote per side of each book                             |
+| BPS            | 100                  | "Radius from index price to begin quoting at, specified in basis points"    |
+| INTRALEVEL_BPS | 100                  | Number of basis points between levels on the same side of the book          |
+| QTY_NOTIONAL   | 5                    | Notional dollar value of lot sizes (using index price)                      |
+
+
+Set your environment variable and run the mm with `yarn start`. For example:
+
+```
+RPC=REPLACE_WTIH_URL_OF_YOUR_RPC TRG=REPLACE_WITH_TRG_PUBKEY yarn start
+```
---
+++ src/mm.js
@@ -0,0 +1,240 @@
+import fs from 'fs';
+import { clusterApiUrl, ComputeBudgetProgram, Connection, Keypair, PublicKey, Transaction } from "@solana/web3.js";
+import { Wallet } from "@project-serum/anchor";
+import dexterityTs from "@hxronetwork/dexterity-ts";
+const dexterity = dexterityTs.default;
+
+var zero = dexterity.Fractional.Zero();
+
+const MAX_COMPUTE_UNITS = 1400000; // 1.4m is solana's max
+
+function getEV(key, otherwise, parseNumba = true) {
+    if (!process.env.hasOwnProperty(key)) {
+        if (typeof otherwise !== 'undefined') {
+            return otherwise;
+        }
+        console.error('The environment variable', key, 'must be set');
+        process.exit();
+    }
+    let v = process.env[key];
+    if (parseNumba) {
+        v = parseInt(v);
+        if (isNaN(v)) {
+            if (typeof otherwise !== 'undefined') {
+                return otherwise;
+            }
+            console.error('The environment variable', key, 'must be set');
+            process.exit();
+        }
+    }
+    return v;
+}
+
+const trgPubkey = new dexterity.web3.PublicKey(getEV('TRG', undefined, false));
+const mpgPubkey = new dexterity.web3.PublicKey(getEV('MPG', '4cKB5xKtDpv4xo6ZxyiEvtyX3HgXzyJUS1Y8hAfoNkMT', false));
+
+// let buf = readJsonFileToUint8Array('../dexterity/target/deploy/mpg-keypair.json');
+// const mpgKp = Keypair.fromSecretKey(buf);
+// const mpgPubkey = mpgKp.publicKey;
+
+
+function readJsonFileToUint8Array(filePath) {
+    const jsonContent = fs.readFileSync(filePath, 'utf8');
+    const jsonArray = JSON.parse(jsonContent);
+    const uint8Array = new Uint8Array(jsonArray);
+    return uint8Array;
+}
+
+const rpc = getEV('RPC', 'http://localhost:8899/', false);
+console.log('rpc:', rpc);
+
+// create a new wallet and airdrop it some SOL
+const keypair = Keypair.fromSecretKey(readJsonFileToUint8Array('my_key.json'));
+const wallet = new Wallet(keypair);
+
+const connection = new Connection(rpc, "confirmed");
+
+// get the latest manifest
+const manifest = await dexterity.getManifest(rpc, false, wallet);
+
+console.log('updating covariance metadatas...');
+await manifest.updateCovarianceMetadatas();
+console.log('success');
+
+const mpgPkStr = mpgPubkey.toBase58();
+console.log('mpg pubkey:', mpgPkStr);
+
+console.log('fetching orderbooks for the desired mpg...');
+await manifest.updateOrderbooks(mpgPubkey);
+console.log('successfully fetched orderbooks for the desired mpg!');
+
+let desiredMpg, desiredOrderbooks;
+for (const [pkStr, { pubkey, mpg, orderbooks }] of manifest.fields.mpgs) {
+    if (mpgPkStr === pkStr) {
+        desiredMpg = mpg;
+        desiredOrderbooks = orderbooks;
+        break;
+    }
+}
+if (!desiredMpg) {
+    console.log('failed to find mpg!', manfiest.fields?.mpg);
+    process.exit();
+}
+console.log('found mpg object!');
+
+const TRG_PUBKEY = new PublicKey(trgPubkey);
+
+// create a trader
+const trader = new dexterity.Trader(manifest, TRG_PUBKEY);
+const holdOffOnVarianceCacheUpdate = new Set();
+
+let lastPortfolioValue = dexterity.Fractional.Zero();
+let lastPositionValue = dexterity.Fractional.Zero();
+let lastNetCash = dexterity.Fractional.Zero();
+let lastPnL = dexterity.Fractional.Zero();
+
+function logTrader() {
+    console.log(
+        "trader [" + trader.traderRiskGroup.toBase58().slice(0, 4) + "]",
+        "Portfolio Value:",
+        trader.getPortfolioValue().toString(),
+        "Position Value:",
+        trader.getPositionValue().toString(),
+        "Net Cash:",
+        trader.getNetCash().toString(),
+        "PnL:",
+        trader.getPnL().toString()
+    );
+}
+
+// define what happens when our trader changes in any way
+const onUpdate = trader => async thing => {
+    try {
+        const portfolioValue = trader.getPortfolioValue();
+        const positionValue = trader.getPositionValue();
+        const netCash = trader.getNetCash();
+        const pnl = trader.getPnL();
+        let somethingChanged = false;
+        if (!portfolioValue.eq(lastPortfolioValue)) {
+            lastPortfolioValue = portfolioValue;
+            somethingChanged = true;
+        }
+        if (!positionValue.eq(lastPositionValue)) {
+            lastPositionValue = positionValue;
+            somethingChanged = true;
+        }
+        if (!netCash.eq(lastNetCash)) {
+            lastNetCash = netCash;
+            somethingChanged = true;
+        }
+        if (!pnl.eq(lastPnL)) {
+            lastPnL = pnl;
+            somethingChanged = true;
+        }
+        if (somethingChanged) {
+            console.log('something changed!');
+            logTrader();
+        }
+    } catch (e) {
+        console.error(e);
+        console.error(e.logs);
+    }
+};
+
+console.log(
+  `Wallet: ${wallet.publicKey.toBase58()} TRG: ${TRG_PUBKEY.toBase58()}`
+);
+
+// call connect() so updates are streamed
+await trader.connect(onUpdate(trader));
+console.log('fetching address lookup table account...');
+await trader.fetchAddressLookupTableAccount();
+console.log('successfully fetched address lookup table account...');
+
+console.log('cancelling all orders at startup...');
+try {
+    await trader.cancelAllOrders([]);
+} catch (e) {
+    console.error('failed to cancel all orders!');
+    console.error(e.logs);
+    console.error(e);
+    process.exit();
+}
+
+function getProductAndMarketState(name) {
+    let p, ms;
+    for (const [productName, { index, product }] of dexterity.Manifest.GetProductsOfMPG(trader.mpg)) {
+        const meta = dexterity.productToMeta(product);
+        if (productName.includes(name)) {
+            p = product.outright.outright;
+            ms = desiredOrderbooks.get(meta.orderbook.toBase58());
+            break;
+        }
+    }
+    if (!p || !ms) {
+        console.log('failed to desired product or desired market state!', p, ms);
+        process.exit();
+    }
+    return { product: p, marketState: ms };
+}
+
+let quotePeriodMs = getEV('QUOTE_PERIOD_MS', 5000);
+let numLevels = getEV('NUM_LEVELS', 5);
+let bps = dexterity.Fractional.New(getEV('BPS', 100), 4);
+let intralevelBps = dexterity.Fractional.New(getEV('INTRALEVEL_BPS', 100), 4);
+let qtyNotional = dexterity.Fractional.New(getEV('QTY_NOTIONAL', 5), 0); // default to $5 notional value order sizes
+
+const productNameFilter = getEV('PRODUCT_NAME_FILTER', '', false);
+
+setInterval(async _ => {
+    const UNINITIALIZED = new dexterity.web3.PublicKey('11111111111111111111111111111111');
+    for (const [productName, obj] of dexterity.Manifest.GetProductsOfMPG(trader.mpg)) {
+        if (productNameFilter !== '' && !productName.includes(productNameFilter)) {
+            continue;
+        }
+        const { index: productIndex, product } = obj;
+        const meta = dexterity.productToMeta(product);
+        if (meta.productKey.equals(UNINITIALIZED)) {
+            continue;
+        }
+        if (product.combo?.combo) {
+            continue;
+        }
+        const index = dexterity.Manifest.GetIndexPrice(trader.markPrices, meta.productKey);
+        const lotSize = qtyNotional.div(index).round_down(new dexterity.BN(meta.baseDecimals));
+        console.log('quoting on', productName, 'around index', index.toString(4, true));
+        let price;
+
+        price = index.mul(dexterity.Fractional.One().add(bps));
+        for (let i = 0; i < numLevels; i++) {
+            const clientOrderId = new dexterity.BN(index*100+i);
+            try {
+                trader.sendV0Tx([
+                    trader.getCancelOrderIx(productIndex, new dexterity.BN(0), true, clientOrderId),
+                    trader.getNewOrderIx(productIndex, false, price, lotSize, false, null, null, clientOrderId)
+                ]);
+            } catch (e) {
+                console.error('failed to send replace for level', i, 'of asks of', productName, 'price', price.toString(4, true), 'qty', lotSize.toString());
+                console.error(e);
+                console.error(e.logs);
+            }
+            price = price.mul(dexterity.Fractional.One().add(intralevelBps));
+        }
+
+        price = index.mul(dexterity.Fractional.One().sub(bps));
+        for (let i = 0; i < numLevels; i++) {
+            const clientOrderId = new dexterity.BN(index*100+numLevels+i);
+            try {
+                trader.sendV0Tx([
+                    trader.getCancelOrderIx(productIndex, new dexterity.BN(0), true, clientOrderId),
+                    trader.getNewOrderIx(productIndex, true, price, lotSize, false, null, null, clientOrderId)
+                ]);
+            } catch (e) {
+                console.error('failed to send replace for level', i, 'of bids of', productName, 'price', price.toString(4, true), 'qty', lotSize.toString());
+                console.error(e);
+                console.error(e.logs);
+            }
+            price = price.mul(dexterity.Fractional.One().sub(intralevelBps));
+        }
+    }
+}, quotePeriodMs);
